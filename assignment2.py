2ddcc2892ddd22bc309769df7f179774d834d1fe
Pranathi <neethu@codingdojo.com>
2017-10-02 20:46:43 -0700

    multiples sum avg

---
+++ assignment2.py
@@ -0,0 +1,21 @@
+for i in range(1, 100, 2):
+    print(i)
+#multiples part1
+
+
+for x in range(5,1000000,5):
+    print(x)
+#multiples part2
+
+
+a = [1, 2, 5, 10, 255, 3] 
+sum=0
+for element in a:
+    sum+=element
+print sum
+#prints sum 
+print len(a)
+c= sum/len(a)
+
+print c
+#prints avg
